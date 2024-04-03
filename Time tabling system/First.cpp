9014d37cec5163488cea3f7684950b9efd49baee
iasimkhan2005 <asimali9596a@gmail.com>
2024-04-03 12:57:59 +0500

    Add project files.

---
+++ Time tabling system/First.cpp
@@ -0,0 +1,18 @@
+#include "First.h"
+
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+int main(array<System::String^>^ args)
+{
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+
+    // Create an instance of your main form
+   Timetablingsystem ::First form;
+    Application::Run(% form);
+
+    return 0;
+}
